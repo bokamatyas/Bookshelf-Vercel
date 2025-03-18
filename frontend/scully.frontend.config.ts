1ac82804267d0b2428f996e9e76a5b1f822391e6
Mátyás Bóka <72026133+bokamatyas@users.noreply.github.com>
2025-03-18 20:19:06 +0100

    Update config

--- frontend/scully.frontend.config.ts
+++ frontend/scully.frontend.config.ts
@@ -6,15 +6,9 @@ export const config: ScullyConfig = {
     distFolder: "./dist/frontend/browser",
     outDir: './dist/static',
     routes: {
-        '/home': {
-            type: 'default'
-        },
-        '/404': {
-            type: 'default'
-        },
-        '**': {
-            type: 'default'
-        }
+        '/home': { type: 'default' },
+        '/404': { type: 'default' },
+        '**': { type: 'default' }
     },
     extraRoutes: [
         '/books',
@@ -25,5 +19,10 @@ export const config: ScullyConfig = {
         '/auth/login',
         '/auth/register',
         '/summary-item'
-      ]
+    ],
+    puppeteerLaunchOptions: {
+      executablePath: process.env['CHROMIUM_PATH'] || '/usr/bin/chromium',
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      timeout: 120000
+    }
 };
